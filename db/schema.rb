01e72e9d9ae9d619e0816c13b4b18576e6eeca4a
yoppy44 <136479890+yoppy44@users.noreply.github.com>
2023-07-13 00:57:58 +0900

    itemsテーブル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_11_090651) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_12_152242) do
+  create_table "items", charset: "utf8", force: :cascade do |t|
+    t.string "item_name", null: false
+    t.integer "price", null: false
+    t.integer "delivery_charge_id", null: false
+    t.text "explanation", null: false
+    t.bigint "user_id", null: false
+    t.integer "category_id", null: false
+    t.integer "quality_id", null: false
+    t.integer "shipping_origin_area_id", null: false
+    t.integer "delivery_date_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_items_on_user_id"
+  end
+
   create_table "users", charset: "utf8", force: :cascade do |t|
     t.string "name", null: false
     t.string "email", default: "", null: false
@@ -29,4 +44,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_11_090651) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "items", "users"
 end
