50eb6e91f8ebb0c1d4e8fb647f5760c769e71d67
yoppy44 <136479890+yoppy44@users.noreply.github.com>
2023-07-13 01:05:33 +0900

    モデルの設定(仮)

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,12 @@
 class Item < ApplicationRecord
-  
+  validates :item_name, presence: true
+  validates :price, presence: true
+  validates :delivery_charge_id, presence: true
+  validates :explanation, presence: true
+  validates :category_id, presence: true
+  validates :quality_id, presence: true
+  validates :shipping_origin_area_id, presence: true
+  validates :delivery_date_id, presence: true
+
+  belongs_to :user
 end
--- db/migrate/20230712152242_create_items.rb
+++ db/migrate/20230712152242_create_items.rb
@@ -1,15 +1,15 @@
 class CreateItems < ActiveRecord::Migration[7.0]
   def change
     create_table :items do |t|
-      t.string :item_name,                null: false
-      t.integer :price,                   null: false
-      t.integer :delivery_charge_id,      null: false
-      t.text :explanation,                null: false
-      t.references :user,                 null: false, foreign_key: true
-      t.integer :category_id,             null: false
-      t.integer :quality_id,              null: false
-      t.integer :shipping_origin_area_id, null: false
-      t.integer :delivery_date_id,        null: false
+      t.string     :item_name,               null: false
+      t.integer    :price,                   null: false
+      t.integer    :delivery_charge_id,      null: false
+      t.text       :explanation,             null: false
+      t.references :user,                    null: false, foreign_key: true
+      t.integer    :category_id,             null: false
+      t.integer    :quality_id,              null: false
+      t.integer    :shipping_origin_area_id, null: false
+      t.integer    :delivery_date_id,        null: false
 
       t.timestamps
     end
