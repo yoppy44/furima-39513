186f3ac12df396b0dfbbae49f0bda79bcba06f78
yoppy44 <136479890+yoppy44@users.noreply.github.com>
2023-07-13 12:47:38 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,4 +9,17 @@ class Item < ApplicationRecord
   belongs_to :quality
   belongs_to :shipping_origin_area
   belongs_to :delivery_date
+
+  validates :item_name, presence: true
+  validates :price, presence: true
+  validates :price,  format: { with: /\A[0-9]+\z/,message: "is invalid. Input half-width characters" }
+  validates :price, format: { with: [3~9][0~9][0~9]|[0~9][0~9][0~9][0~9][0~9][0~9][0~9],message: "is out of setting range" }
+  validates :delivery_charge_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :explanation, presence: true
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :quality_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :shipping_origin_area_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :delivery_date_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :image, presence: true
+
 end
