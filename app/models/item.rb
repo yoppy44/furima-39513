3ff05ef19de15fc74e004bc076ba1be2198ff96b
yoppy44 <136479890+yoppy44@users.noreply.github.com>
2023-07-13 12:10:21 +0900

    ActiveHashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,11 @@ class Item < ApplicationRecord
 
   belongs_to :user
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :delivery_charge
+  belongs_to :category
+  belongs_to :quality
+  belongs_to :shipping_origin_area
+  belongs_to :delivery_date
 end
