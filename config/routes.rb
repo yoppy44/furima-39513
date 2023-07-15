081b200d830c6076c9fefab3f682a562da43927a
yoppy44 <136479890+yoppy44@users.noreply.github.com>
2023-07-15 20:46:38 +0900

    商品詳細ページへのルート設定

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   root to: "items#index"
-  resources :items, only: [:index, :new, :create]
+  resources :items, only: [:index, :new, :create, :show]
 end
